b4bfbc4797911ddba2f54c5b8fb302956ab9f4fd
GragaBot <zitengxu@gmail.com>
2016-10-28 07:21:57 -0400

    Initial Commit

---
+++ FashionRaffle/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FashionRaffle
+//
+//  Created by Mac on 2016/10/28.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
